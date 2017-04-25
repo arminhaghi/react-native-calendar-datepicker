b36095f466a975dc23d88135a0236ad487e7f6e9
Haghi, Armin <Armin.Haghi@nordstrom.com>
2017-04-24 23:25:09 -0700

    Updated has events to work with Moment

--- src/pure/DaySelector.react.js
+++ src/pure/DaySelector.react.js
@@ -169,7 +169,7 @@ export default class DaySelector extends Component {
         date: iterator.date(),
         selected: props.selected && iterator.isSame(props.selected, 'day'),
         today: iterator.isSame(Moment(), 'day'),
-        hasEvent: _.some(props.eventDates, eventDate => iterator.toDate().getTime() === eventDate.getTime()),
+        hasEvent: _.some(props.eventDates, eventDate => iterator.format("YYYY MM DD") === eventDate.format("YYYY MM DD"))
       };
       // Add it to the result here.
       iterator.add(1, 'day');
